771e8f2a68a774d6b46c1eb37e332e2a9c4afb0d
randike12 <rasaurabh1144@gmail.com>
2025-03-21 06:59:41 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7606807536:AAEzWoRCzZIUU8-8Pz9fIL9A6Z3efqCy1UU")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@RAJPUTDDOS")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1821595166")
+
+
+
+
+
+
+
+  
